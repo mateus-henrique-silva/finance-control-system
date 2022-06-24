606816d60806dd9469caef51c84f1b39cdd5f419
mateus-henrique-silva <henrique.s@gsuite.com.br>
2022-06-24 11:29:15 -0300

    test styled-components

---
+++ finance/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Global from "./styles/global";
+
+const App = () => {
+  return (
+    <>
+      <div>App</div>
+      <Global />
+    </>
+  );
+};
+
+export default App;
---
+++ finance/src/styles/global.js
@@ -0,0 +1,14 @@
+import {createGlobalStyle} from "style-components";
+
+const Global = createGlobalStyle`
+
+body{
+    margin: 0;
+    padding: 0;
+    font-family:'Poppins' sans-serif;
+    background-color:#f2f2f2 !important;
+}
+
+`;
+
+export default Global;
